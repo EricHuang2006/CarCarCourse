c4785c9f724955795848a775089ebb2754ef05d9
phoenix0423 <ericabcd423@gmail.com>
2025-04-17 00:08:51 +0800

    update cross_test

--- test/cross_test/cross_test.ino
+++ test/cross_test/cross_test.ino
@@ -16,13 +16,22 @@ void setup() {
     Serial.begin(9600);
     BT.begin(9600);
     for(int i = 0; i < 2; i++){
-        pinMode(PWM[i], OUTPUT);
-        pinMode(A[i], OUTPUT);
-        pinMode(B[i], OUTPUT);
+      pinMode(PWM[i], OUTPUT);
+      pinMode(A[i], OUTPUT);
+      pinMode(B[i], OUTPUT);
     }
 }
 
+int ck(){
+    int c = 0;
+    for(int i = 0; i < 5; i++) c += digitalRead(RF[i]);
+    return c;
+}
+
+int lst_vl = 0, lst_vr = 0;
+
 void Writemotor(double a, double b){
+    lst_vl = a, lst_vr = b; 
     analogWrite(PWM[0], abs(a));
     analogWrite(PWM[1], abs(b));
     digitalWrite(A[0], a <= 0);
@@ -31,49 +40,115 @@ void Writemotor(double a, double b){
     digitalWrite(B[1], b <= 0);
 }
 
-void tracking(){
-    double error = 0;
-    for(int i = 0; i < 5; i++) error += (i - 2) * digitalRead(RF[i]);
-    int kp = 100, tp = 255;
-    int vr = tp - kp * error ;
-    int vl = tp + kp * error ;
-    vl = min(vl, 255), vr = min(vr, 255);
-    vl = max(vl, -255), vr = max(vr, -255);
-    Writemotor(vl, vr);
+
+// void tracking(){
+  //   static int ll = 0, lr = 0;
+  //   double error = 0;
+  //   for(int i = 0; i < 5; i++) error += arr[i] * digitalRead(RF[i]);
+  //   int kp = 35, tp = 160;
+  //   int vr = tp - kp * error;
+  //   int vl = 0.95 * (tp + kp * error);
+  //   int deduct = digitalRead(RF[0]) | digitalRead(RF[4]);
+//   if(deduct) vl *= 0.8, vr *= 0.8;
+//   // vl = (vl + 4 * ll) / 10, vr = (vr + 9 * lr) / 10;
+//   vl = min(vl, 255), vr = min(vr, 255);
+//   vl = max(vl, -255), vr = max(vr, -255);
+//   ll = vl, lr = vr;
+//   Writemotor(vl, vr);
+// }
+
+// double arr[] = {-1.8, -1, 0, 1, 1.8};
+// double arr[] = {-3, -1, 0, 1, 3};
+double arr[] = {-60, -20, 0, 20, 60};
+
+void tracking(bool flag = false){ // PID Control
+  static double kd = 0, lastError = 0, ki = 0, sumError = 0;
+  if(flag) lastError = 0, sumError = 0;
+  double error = 0;
+  for(int i = 0; i < 5; i++) error += arr[i] * digitalRead(RF[i]);
+  sumError += error; // I
+  sumError = constrain(sumError, -20, 20); // max speed : [-200, 200]
+  double dError = error - lastError; // D
+  
+  int kp = 1, tp = 170; // old : 37
+  // int kp = 22, tp = 90;
+  int powerCorrection = kp * error + kd * dError + ki * sumError;
+  lastError = error;
+  int vr = (tp - powerCorrection) * (powerCorrection >= 0 ? 1 : 1);
+  int vl = 0.95 * (tp + powerCorrection) * (powerCorrection <= 0 ? 1 : 1);
+  // int vl = 0.95 * (tp + powerCorrection) * (powerCorrection <= 0 ? 1 : 0.75);
+  // int vr = (tp - powerCorrection) * (powerCorrection >= 0 ? 1 : 0.75);
+  // int deduct = digitalRead(RF[0]) | digitalRead(RF[4]);
+  // if(deduct) vl *= 0.85, vr *= 0.85;
+  vl = min(vl, 255), vr = min(vr, 255);
+  vl = max(vl, -255), vr = max(vr, -255);
+  Writemotor(vl, vr);
 }
 
-void TurnLeft(unsigned long wait_ms){
+void stop(double t = 100){
   unsigned long start_time = millis();
-  while(millis()-start_time < wait_ms){
-    Writemotor(85,255);
+  start_time = millis();
+  while(millis() - start_time < t){
+    Writemotor(0, 0);
   }
-  Writemotor(255,255);
+}
+void TurnLeft(unsigned long wait_ms = 280){
+  unsigned long st = millis();
+  while(millis() - st < 90){
+    Writemotor(40, 45);
+  }
+  st = millis();
+  while(millis() - st < wait_ms){
+    Writemotor(-40, 100);
+  }
+  while(!digitalRead(RF[0])){
+    Writemotor(-30, 50);
+  }
+  stop(150);
+  // st = millis();
+  // while(millis() - st < 50){
+  //   Writemotor(100, 0);
+  //   if(digitalRead(RF[2])) tracking();
+  // }
 }
 
-void TurnRight(unsigned long wait_ms){
+
+void TurnRight(unsigned long wait_ms = 280){
+  unsigned long st = millis();
+  while(millis() - st < 90){
+    Writemotor(40, 40);
+  }
+  st = millis();
+  while(millis() - st < wait_ms){
+    Writemotor(110, -50);
+  }
+  while(!digitalRead(RF[4])){
+    Writemotor(48, -45);
+  }
+  stop(150);
+  // st = millis();
+  // while(millis() - st < 50){
+  //   Writemotor(0, 110);
+  //   if(digitalRead(RF[2])) tracking();
+  // }
+}
+
+void UTurn(unsigned long wait_ms = 440){
   unsigned long start_time = millis();
+  start_time = millis();
   while(millis()-start_time < wait_ms){
-    Writemotor(255,85);
+    Writemotor(80, -200);
   }
-  Writemotor(255,255);
+  while(!digitalRead(RF[4])){
+    Writemotor(28, -70);
+  }
+  stop(200);
 }
 
-void UTurn(unsigned long wait_ms){
+void Forward(){
   while(ck() == 5){
-    Writemotor(255, 255);
+    Writemotor(130, 138);
   }
-  unsigned long start_time = millis();
-  while(millis()-start_time < wait_ms){
-    //Writemotor(175,-175); 700
-    Writemotor(225,-225);
-  }
-  Writemotor(255,255);
-}
-
-int ck(){
-    int c = 0;
-    for(int i = 0; i < 5; i++) c += digitalRead(RF[i]);
-    return c;
 }
 
 int state = 0;
@@ -83,7 +158,7 @@ bool checkMFRC(){
     if(!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
         return false;
     } //PICC_IsNewCardPresent()：是否感應到新的卡片?
-    Serial.println(F("**Card Detected:**"));
+    Serial.println(F("*Card Detected:*"));
     // mfrc522->PICC_DumpDetailsToSerial(&(mfrc522->uid)); //讀出 UID
     for (byte i = 0; i < mfrc522.uid.size; i++) {
         Serial.print(mfrc522.uid.uidByte[i], HEX);
@@ -95,7 +170,7 @@ bool checkMFRC(){
     mfrc522.PICC_HaltA(); // 讓同一張卡片進入停止模式 (只顯示一次)
     mfrc522.PCD_StopCrypto1(); // 停止 Crypto1
     return true;
-}
+  }
 
 char BT_get(){
     if(BT.available()){
@@ -103,49 +178,67 @@ char BT_get(){
         Serial.print("get : ");
         Serial.println(c);
         if(c == 's'){
-            Serial.print("same!\n");
-            Writemotor(150, -150);
-            delay(500);
-            while(true){
-                Writemotor(0, 0);
-            }
-            Serial.print("OK!\n");
+            Serial.print("stop!\n");
         }
         return c;
     }
     return '0';
 }
 
+void test1(){
+  if(ck() == 5){
+    if(state == 0){ // 起點 -> 1，之後右轉
+      while(ck() >= 3){
+        Writemotor(153, 160);
+      }
+    }
+    else{
+      UTurn();
+      state = -1;
+    }
+    state++;
+  }
+}
 void loop() {
-    checkMFRC();
-    BT_get();
+    // checkMFRC();
+    // BT_get();
     tracking();
+    if(state == 5){
+      mfrc522.PCD_Init();
+      if(checkMFRC()){
+          while(true){
+              Writemotor(0, 0);
+          }
+      }
+    }
+    // test1();
+    // return;
     if(ck() == 5){
         if(state == 0){ // 起點 -> 1，之後右轉
-          TurnRight(650);
+          TurnRight();
         }
         else if (state == 1){ // 1 -> 2，之後迴轉
-          UTurn(575);
+          UTurn();
         }
         else if(state == 2){ // 2 -> 1，之後直走
-            while(ck() == 5){
-                Writemotor(255, 255);
-            }
+          Forward();
         }
         else if(state == 3){ // 1 -> 4，之後迴轉
-          UTurn(575);
+          UTurn();
         }
         else if(state == 4){ // 4 -> 1，之後左轉
-          TurnLeft(650);
+          TurnLeft();
         }
         else{ // 1 -> 3，之後停止
+          UTurn();
+          state = 0;
+          return;
             while(ck()){
-                Writemotor(255, 255);
+                Writemotor(200, 200);
             }
         }
         state++;
     }
-    tracking();
     if(state > 5){
       Writemotor(0,0);
       return;
